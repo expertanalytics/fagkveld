44abf07b7052a5d9e0ff8b10c093af5eca2535fe
Ola Skavhaug <ola@xal.no>
2017-07-03 17:29:28 +0200

    Add polygon data fetcher

--- verdenskart/src/utils/data_fetcher.py
+++ verdenskart/src/utils/data_fetcher.py
@@ -1,13 +1,33 @@
+from typing import Dict, Any
 import logging
 import requests
 
 
 def get_world_topology():
-    ans = requests.get("https://restcountries.eu/rest/v2/all?fields=name;capital;region;subregion;latlng;alpha3Code;borders")
+    ans = requests.get("https://restcountries.eu/rest/v2/all?fields=name;capital;region;subregion;latlng;alpha2Code;alpha3Code;borders")
     if ans.status_code != 200:
         raise RuntimeError("Unable to get country data")
     countries = {country.pop("alpha3Code"): country for country in ans.json()}
     logging.info("Done getting raw topological data")
-    for country in countries:
-        countries[country]["borders"] = [countries[name] for name in countries[country]["borders"]]
     return countries
+
+
+def get_country_polygon(alpha2code: str) -> Dict[str, Any]:
+    try:
+        ans = requests.get("http://nominatim.openstreetmap.org/search?country={}&polygon_geojson=1&format=json".format(alpha2code.lower()))
+        if ans.status_code != 200:
+            raise RuntimeError("Unable to get boundary for {}".format(country))
+        res = ans.json()
+        if res:
+            return res[0]["geojson"]
+        else:
+            print("Missing data for {}".format(alpha2code))
+    except requests.exceptions.ChunkedEncodingError as err:
+        print("Got error while getting polygon for".format(alpha2code))
+
+
+#if __name__ == "__main__":
+#    topology = get_world_topology()
+#    for c in topology.values():
+#        c.pop("borders")
+#        res = get_country_polygon(c["alpha2Code"])
