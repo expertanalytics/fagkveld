18688fb65245ddfd371568a5fb25ba28754951ef
Jonathan Feinberg <jonathf@gmail.com>
2017-07-03 13:20:45 +0200

    added PQ-projection and Hybrid colors

---
+++ verdenskart/src/utils/colors_hybrid.py
@@ -0,0 +1,18 @@
+COLORS = {
+    "color0": "#2d3c46",
+    "color1": "#a54242",
+    "color2": "#8c9440",
+    "color3": "#de935f",
+    "color4": "#f5819d",
+    "color5": "#85678f",
+    "color6": "#5e8d87",
+    "color7": "#6c7a80",
+    "color8": "#425059",
+    "color9": "#cc6666",
+    "color10": "#b5bd68",
+    "color11": "#f0c674",
+    "color12": "#81a2be",
+    "color13": "#b294bb",
+    "color14": "#8abeb7",
+    "color15": "#c5c8c6",
+}
