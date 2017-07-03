b17f938763766fcba74d8f643bd1dc4ccabd7be2
Vinzenz Gregor Eck <vinzenz@eck-mail.eu>
2017-07-03 16:26:37 +0200

    merged

--- verdenskart/src/model/location.py
+++ verdenskart/src/model/location.py
@@ -4,7 +4,7 @@ from abc import abstractmethod
 import numpy
 
 from bokeh.models import Model
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Patches, LabelSet
 
 
 class Location:
@@ -19,6 +19,16 @@ class Location:
     children: "Locations" = {}
     level: int = 0
 
+    # bokeh
+    border_ds_keys = ('xs', 'ys')
+    border_ds = ColumnDataSource({k: [] for k in border_ds_keys})
+    border_glyph = None
+
+    location_ds_keys = ('name')
+    location_ds = ColumnDataSource({k: [] for k in border_ds_keys})
+    location_glyph = None
+
+
     @property
     def location_x(self) -> float:
         pass
@@ -32,7 +42,18 @@ class Location:
     @property
     @abstractmethod
     def visuals(self) -> List[Tuple[Model, ColumnDataSource]]:
-        pass
-        # TODO: implement here
+        # border
+        self.border_ds = ColumnDataSource({'xs': self.border_x,
+                                           'ys': self.border_y})
+        self.border_glyph = Patches(xs='xs', ys='ys', source=self.border_ds)
+
+        # name at location x,y
+
+        return [(self.border_glyph, self.border_ds)]
+
+    @property
+    def clear_visuals(self):
+        self.border_ds.data = {k: [] for k in self.border_ds_keys}
+        self.location_ds.data = {k: [] for k in self.border_ds_keys}
 
 Locations = Dict[str, Location]
