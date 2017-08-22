96e16b9610e8b60d36425e7bc5435d266de1f8bf
Jonathan Feinberg <jonathf@gmail.com>
2017-08-22 17:31:22 +0200

    bugfix: missing multipolygon

--- worldmap/src/worldmap/utils/builder.py
+++ worldmap/src/worldmap/utils/builder.py
@@ -15,7 +15,7 @@ HIERARKY = {
 
 def build_group(
         location_group: str,
-        loction_class: Type[Location] = Location,
+        location_class: Type[Location] = Location,
 ) -> Dict[str, Location]:
     """
     Build group (like country or cities).
@@ -44,6 +44,22 @@ def build_group(
             coordinates = numpy.array(shape["geometry"]["coordinates"])
             location.border_x.append(coordinates.T[0])
             location.border_y.append(coordinates.T[1])
+            location.border_hull_x.append(None)
+            location.border_hull_y.append(None)
+
+        elif shape["geometry"]["type"] in ("MultiPolygon",):
+            for coordinates in shape["geometry"]["coordinates"]:
+                border = numpy.array(coordinates[0])
+                location.border_x.append(border.T[0])
+                location.border_y.append(border.T[1])
+
+                if len(coordinates) == 2:
+                    hull = numpy.array(coordinates[1])
+                    location.border_hull_x.append(hull.T[0])
+                    location.border_hull_y.append(hull.T[1])
+                else:
+                    location.border_hull_x.append(None)
+                    location.border_hull_y.append(None)
 
         location.shape = shape
         groups[name] = location
@@ -51,7 +67,9 @@ def build_group(
     return groups
 
 
-def build_coastlines(loction_class: Type[Location] = Location):
+def build_coastlines(
+        loction_class: Type[Location] = Location
+) -> Dict[str, Location]:
     return build_group("coastline", loction_class)
 
 def build_countries(loction_class: Type[Location] = Location):
